f0b8d4497df5fc0c79721da670ba304ad5e2adc7
JeffPatten <jeffspatten@gmail.com>
2023-11-02 10:34:20 -0500

    added Week 1 Day 3 recoeding link

---
+++ week_1/day_4/functionsAndDebugging.js
@@ -0,0 +1,23 @@
+let myArr = ["bbq", "chili", "donuts"];
+let myArr2 = ["enchiladas", "tacos", "pizza"];
+
+// for (let i =0; i < myArr.length; i ++) {
+//     console.log(myArr[i])
+// }
+
+// for (let i = 0; i < myArr2.length; i ++) {
+//     console.log(myArr2[i])
+// }
+
+let i = 0;
+while (i < 10) {
+    console.log(myArr[i])
+    i++
+}
+console.log("This is i:", i);
+
+i = 0;
+while (i < 10) {
+    console.log(myArr2[i])
+    i++
+}
