8e41c59af6f80113dd196dc1a5c032ebe36b28a8
JeffPatten <jeffspatten@gmail.com>
2023-11-03 10:56:46 -0500

    adding day 4 code examples

--- week_1/day_4/functionsAndDebugging.js
+++ week_1/day_4/functionsAndDebugging.js
@@ -1,13 +1,13 @@
 let myArr = ["bbq", "chili", "donuts"];
 let myArr2 = ["enchiladas", "tacos", "pizza"];
 
-// for (let i =0; i < myArr.length; i ++) {
-//     console.log(myArr[i])
-// }
+for (let i =0; i < myArr.length; i ++) {
+    console.log(myArr[i])
+}
 
-// for (let i = 0; i < myArr2.length; i ++) {
-//     console.log(myArr2[i])
-// }
+for (let i = 0; i < myArr2.length; i ++) {
+    console.log(myArr2[i])
+}
 
 let i = 0;
 while (i < 10) {
@@ -21,3 +21,23 @@ while (i < 10) {
     console.log(myArr2[i])
     i++
 }
+
+function sayHello() {
+    console.log("Hello!");
+    // no return statement, so returns undefined
+  }
+
+let whatAmISaying = sayHello();
+console.log(whatAmISaying);
+
+function calculateTotalWithTax(price) {
+    console.log(price);
+    return price + price * tax;
+}
+
+console.log(calculateTotalWithTax(4)); // 4.3
+
+const total = calculateTotalWithTax(4);
+console.log(price);  // error!
+console.log(total);  
+
